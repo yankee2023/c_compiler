59ad3aab37037a600d4c911ab04af91262ddfce8
yankee2023 <tad.training2023@gamil.com>
2023-10-08 14:53:11 +0900

    不要コメント削除

--- include/9cc.h
+++ include/9cc.h
@@ -2,21 +2,19 @@
 #define _9CC_
 
 /* ----- Enum Definition----- */
-// トークンの種類
 typedef enum {
-  TK_RESERVED, // 記号
-  TK_NUM,      // 整数トークン
-  TK_EOF,      // 入力の終わりを表すトークン
+  TK_RESERVED, 
+  TK_NUM,
+  TK_EOF,
 } TokenKind;
 
 /* ----- Struct Definition----- */
 typedef struct Token Token;
-// トークン型
 struct Token {
-  TokenKind kind; // トークンの型
-  Token *next;    // 次の入力トークン
+  TokenKind kind;
   int val;        // kindがTK_NUMの場合、その数値
   char *str;      // トークン文字列
+  Token *next;
 };
 
 /* ----- Gloval Variable ----- */
