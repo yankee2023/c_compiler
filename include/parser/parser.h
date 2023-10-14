6cb69316f05854fd1c6bbf27b6d4455c2ded8617
yankee2023 <tad.training2023@gamil.com>
2023-10-14 14:55:08 +0900

    AST作成のためノード追加

--- include/parser/parser.h
+++ include/parser/parser.h
@@ -1,6 +1,22 @@
 #ifndef _PARSER_
 #define _PARSER_
 
+typedef enum {
+    ND_ADD,     // +
+    ND_SUB,     // -
+    ND_MUL,     // *
+    ND_DIV,     // /
+    DN_NUM,     // 整数
+} NodeKind;
+
+typedef struct Node Node;
+struct Node {
+    NodeKind kind;  // ノードの型
+    Node* lhs;      // 左辺(left hand side)
+    Node* rhs;      // 右辺(right hand side)
+    int value;      // kindがND_NUMの場合のみ使う
+};
+
 void parse();
 
 #endif
