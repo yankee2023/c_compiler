700dc94550b2dc38fa1b0cfd161d30229219eb5c
yankee2023 <tad.training2023@gamil.com>
2023-10-28 15:44:59 +0900

    エラーメッセージ追加、テストケースの行末に";"追加

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ -s bin/c_compiler ]; then
+if [ -d bin/ ]; then
 	cd bin
 else
-	exit1
+	echo "Can not find bin directory."
+	exit 1
 fi
 
 assert() {
@@ -23,34 +24,38 @@ assert() {
 	fi
 }
 
-assert	0	0
-assert	42	42
-assert	21	"5+20-4"
-assert	41	" 12 + 34 - 5 "
-assert	47	"5+6*7"
-assert	15	"5*(9-6)"
-assert	4	"(3+5)/2"
-assert	10	"-10+20"
+assert	0	"0;"
+assert	42	"42;"
+assert	21	"5+20-4;"
+assert	41	" 12 + 34 - 5;"
+assert	47	"5+6*7;"
+assert	15	"5*(9-6);"
+assert	4	"(3+5)/2;"
+assert	10	"-10+20;"
 
 # 比較演算子
 echo "0=false, 1=true"
-assert	0	"0==1"
-assert	1	"42==42"
-assert	1	"0!=1"
-assert	0	"42!=42"
+assert	0	"0==1;"
+assert	1	"42==42;"
+assert	1	"0!=1;"
+assert	0	"42!=42;"
 
-assert	1	"0<1"
-assert	0	"1<1"
-assert	0	"2<1"
-assert	1	"0<=1"
-assert	1	"1<=1"
-assert	0	"2<=1"
+assert	1	"0<1;"
+assert	0	"1<1;"
+assert	0	"2<1;"
+assert	1	"0<=1;"
+assert	1	"1<=1;"
+assert	0	"2<=1;"
 
-assert	1	"1>0"
-assert	0	"1>1"
-assert	0	"1>2"
-assert	1	"1>=0"
-assert	1	"1>=1"
-assert	0	"1>=2"
+assert	1	"1>0;"
+assert	0	"1>1;"
+assert	0	"1>2;"
+assert	1	"1>=0;"
+assert	1	"1>=1;"
+assert	0	"1>=2;"
+
+assert	14	"a = 3;
+			 b = 5 * 6 - 8;
+			 a + b / 2;"
 
 echo OK
