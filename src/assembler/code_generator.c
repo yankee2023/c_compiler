a8a68666384810b4523b4e5ead1f0d551a5ee187
yankee2023 <tad.training2023@gamil.com>
2023-10-28 15:48:29 +0900

    1字のローカル変数有効化に伴う処理追加

--- src/assembler/code_generator.c
+++ src/assembler/code_generator.c
@@ -4,17 +4,29 @@
 #include "assembler/code_generator.h"
 #include "logger/log.h"
 #include "ast/parser.h"
+#include "tokenizer/tokenizer.h"
 
 static FILE* s_fp;
 
-static void parse_node(Node* node) {
+static void gen_lval(Node *node) {
+    if (node->kind != ND_LVAR) {
+        log_error("代入の左辺値が変数ではありません");
+        exit(1);
+    }
+
+    fprintf(s_fp, "  mov rax, rbp\n");
+    fprintf(s_fp, "  sub rax, %d\n", node->offset);
+    fprintf(s_fp, "  push rax\n");
+}
+
+static void gen(Node* node) {
     if (ND_NUM == node->kind) {
         fprintf(s_fp, "  push %d\n", node->value);
         return;
     }
 
-    parse_node(node->lhs);
-    parse_node(node->rhs);
+    gen(node->lhs);
+    gen(node->rhs);
 
     fprintf(s_fp, "    pop rdi\n");
     fprintf(s_fp, "  pop rax\n");
@@ -53,6 +65,15 @@ static void parse_node(Node* node) {
         fprintf(s_fp, "  setle al\n");
         fprintf(s_fp, "  movzb rax, al\n");
         break;
+    case ND_LVAR:
+        gen_lval(node);
+        printf("  pop rax\n");
+        printf("  mov rax, [rax]\n");
+        printf("  push rax\n");
+        break;
+    case ND_ASSIGN:
+        gen_lval(node->lhs);
+        gen(node->rhs);
     default:
         break;
     }
@@ -73,12 +94,26 @@ void code_gen(Node* node) {
     fprintf(s_fp, ".globl main\n");
     fprintf(s_fp, "main:\n");
 
-    parse_node(node);
+    // プロローグ
+    // 変数26個分の領域を確保する
+    printf("  push rbp\n");
+    printf("  mov rbp, rsp\n");
+    printf("  sub rsp, 208\n");
 
-    // スタックトップに式全体の値が残っているはずなので
-    // それをRAXにロードして関数からの返り値とする
-    fprintf(s_fp, "    pop rax\n");
-    fprintf(s_fp, "    ret\n");
+    // 先頭の式から順にコード生成
+    for (int i = 0; g_code[i]; i++) {
+        gen(g_code[i]);
+
+        // 式の評価結果としてスタックに一つの値が残っている
+        // はずなので、スタックが溢れないようにポップしておく
+        fprintf(s_fp, "  pop rax\n");
+    }
+
+    // エピローグ
+    // 最後の式の結果がRAXに残っているのでそれが返り値になる
+    fprintf(s_fp, "  mov rsp, rbp\n");
+    fprintf(s_fp, "  pop rbp\n");
+    fprintf(s_fp, "  ret\n");
     fprintf(s_fp, "\n");
 
     fclose(s_fp);
